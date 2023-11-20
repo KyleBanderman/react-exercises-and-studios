955694ab792065b4447f18870ed0190c91ee6f71
Kyle Banderman <kyle.banderman4675@gmail.com>
2023-11-20 17:18:16 -0600

    completed the exercise

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Video Games</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>Dead by Daylight is an asymmetric horror survival game with 1 killer and 4 survivors.</li>
+            <li>There are 33 killers in the game.</li>
+            <li>My main killer is The Singularity.</li>
          </ol>
       </div>
    );
